b1927ff5987c6cd32deffd06a391d32dc1c44100
ShaneRSmith <stonefenrir@gmail.com>
2015-09-09 16:25:23 -0500

    Modified trash script to be much cleaner.

--- TRSHIT.sh
+++ TRSHIT.sh
@@ -3,10 +3,6 @@
 echo -n "Enter the name of the file you wish to move to the 'trash' bin including file extension. > "
 read name
 mkdir -p $HOME/TRASH
-if [ ! -e "$name" ];then echo "That file doesn't seem to exist."; fi
-for filename in *; do
-	case "${filename,,*}" in
-	$name*) mv "$filename" "$HOME/TRASH";;
-	*) ;;
-	esac
-done
+if [ ! -e "$name" ];then echo "That file doesn't seem to exist."
+else mv "$name" "$HOME/TRASH" 
+fi
